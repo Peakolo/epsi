305108682dc5ba0e8b28297f26821a559fc4d984
Peakolo <163412512+Peakolo@users.noreply.github.com>
2024-09-24 15:32:59 +0200

    Update connexion2.php
    changement mdp

--- connexion2.php
+++ connexion2.php
@@ -2,7 +2,7 @@
 
 try
 {
-    $con = new PDO('mysql:host=localhost;dbname=ticket;charset=utf8','root','');
+    $con = new PDO('mysql:host=localhost;dbname=ticket;charset=utf8','root','Olympicode');
 
 } catch (Exception $e) {
     die('Erreur :'. $e->getMessage());
