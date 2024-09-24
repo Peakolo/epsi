4e6cae37277e741ddb2b4807a44518d6254ba17b
Peakolo <163412512+Peakolo@users.noreply.github.com>
2024-09-24 15:32:27 +0200

    Update connexion.php
    Changement de mdp

--- connexion.php
+++ connexion.php
@@ -1,6 +1,6 @@
 <?php
        $db_username = 'root';
-       $db_password = '';
+       $db_password = 'Olympicode';
        $db_name = 'ticket';
        $db_host = 'localhost';
        $db = mysqli_connect($db_host, $db_username, $db_password,$db_name)
